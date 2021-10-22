aeb258fc696196c71e5a6e1c58537b668ccd34ec
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-22 06:52:48 +0200

    m11q: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 0 Manufacturer: samsung Model: SM-M115F Script version: 2.0.1 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_m11q.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_m11q-user \
+    omni_m11q-userdebug \
+    omni_m11q-eng
